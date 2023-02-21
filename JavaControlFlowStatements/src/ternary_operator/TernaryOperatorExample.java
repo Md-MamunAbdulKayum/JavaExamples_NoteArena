e5ba0d95285ccb45fb9d07bae98154cce2aec072
Md-MamunAbdulKayum <souravcuetcse2010@gmail.com>
2023-02-21 00:33:15 -0500

    added code for Java ternary operator

---
+++ JavaControlFlowStatements/src/ternary_operator/TernaryOperatorExample.java
@@ -0,0 +1,15 @@
+package ternary_operator;
+
+/**
+ * Java Ternary Operator Example
+ * 
+ * @author Mamun Kayum
+ *
+ */
+public class TernaryOperatorExample {
+	public static void main(String[] args) {
+		int num = 5;
+		String result = (num > 0) ? "positive" : "negative";
+		System.out.println("The number is " + result);
+	}
+}
